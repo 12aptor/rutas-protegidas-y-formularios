9ae4642db1508c02bd43b1b3139445fa470f0e36
Paolo <paholo2054@gmail.com>
2022-09-28 20:35:26 -0500

    update signin function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles.css";
 
 const Home = () => {
   // Estado que contiene nuestras credenciales de usuario
@@ -9,27 +10,36 @@ const Home = () => {
   // Función para ejecutar el evento onSubmit y consumir la API
   const signIn = (e) => {
     e.preventDefault();
-    fetch("https://reqres.in/api/login")
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.parse(user),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
   // Función controlada, para obtener valores de los input
   const handleInputChange = (e) => {
     setUser({
       ...user,
-      [e.currentTarget.name]: e.currentTarget.value
-    })
-  }
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
 
   return (
     <div className="App">
       <form onSubmit={signIn}>
-        <label>
-          Email
+        <h2>Sign In</h2>
+        <div className="form_group">
+          <label>Email</label>
           <input type="email" name="email" onChange={handleInputChange} />
-        </label>
-        <label>
-          Password
+        </div>
+        <div className="form_group">
+          <label>Password</label>
           <input type="password" name="password" onChange={handleInputChange} />
-        </label>
+        </div>
         <button type="submit">Sign In</button>
       </form>
     </div>
