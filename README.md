56c4c73bd115dc04f5014216c3b5e394b28dda00
Paolo <paholo2054@gmail.com>
2022-09-28 21:39:28 -0500

    user controlled functions form

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Rutas protegidas
 
-## Credenciales de prueba
+## Crear un proyecto con vite
+```bash
+npm create vite@latest
+React
+Javascript
+```
 
+## Instalar las dependencias
+```bash
+npm install
+npm install react-router-dom
+```
+
+## Para correr la aplicación
+```bash
+npm run dev
+```
+
+## Credenciales de prueba
 ```json
 {
   "email": "eve.holt@reqres.in",
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,11 +3,69 @@ import { Navigate } from "react-router-dom";
 import { isAuthenticated } from "../services/authServices";
 
 const Users = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    age: 0,
+    birth: "",
+    peruvian: true,
+    height: 0.0,
+  });
+
+  const sendUserData = (e) => {
+    e.preventDefault();
+    console.log(userData);
+  };
+
+  const handleInputChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
   if (!isAuthenticated) {
     return <Navigate to="/" />;
   }
 
-  return <div>Users</div>;
+  return (
+    <div className="App">
+      <form onSubmit={sendUserData}>
+        <h2>User data</h2>
+        <div className="form_group">
+          <label>Name</label>
+          <input type="text" name="name" onChange={handleInputChange} />
+        </div>
+        <div className="form_group">
+          <label>Age</label>
+          <input
+            type="number"
+            min={0}
+            step={1}
+            name="age"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form_group">
+          <label>Birth</label>
+          <input type={"datetime"} name="birth" onChange={handleInputChange} />
+        </div>
+        <div className="form_group">
+          <label>Peruvian</label>
+          <input type="checkbox" name="peruvian" onChange={handleInputChange} />
+        </div>
+        <div className="form_group">
+          <label>Height</label>
+          <input
+            type="number"
+            min={0}
+            step={0.01}
+            name="height"
+            onChange={handleInputChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Users;
